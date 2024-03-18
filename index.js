91e584c300ee18dfc4c89e5ef8ddef1d45993b3b
Marcus Lo <marcusklo1010@gmail.com>
2024-03-17 20:21:01 -0600

    dom modifying elements complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.parentNode.removeChild(mainElement);
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Marcus is the champion';
+newHeader.setAttribute('id', 'victory');
